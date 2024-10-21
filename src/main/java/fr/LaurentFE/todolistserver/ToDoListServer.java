73fca57fa3cb85e0720e2536e19ee77bcdca171a
Mori <laurent.ferric@gmail.com>
2024-10-21 14:47:20 +0200

    Added missing column in table items (is_checked)
    New classes ToDoList & ListItem :
    ToDoList contains its identifier and label, and a list of all items it's composed of
    ListItem contains an item's id, label, and whether it's checked or not
    
    ToDoListServer now can fetch from DB all the to do lists for a given username, or a specific to do list given its name and username.

--- src/main/java/fr/LaurentFE/todolistserver/ToDoListServer.java
+++ src/main/java/fr/LaurentFE/todolistserver/ToDoListServer.java
@@ -1,25 +1,117 @@
 package fr.LaurentFE.todolistserver;
 
 import java.sql.*;
-import java.util.HashMap;
+import java.util.ArrayList;
 
 public class ToDoListServer {
 
     private final Connection connection;
-    Statement statement;
 
     public ToDoListServer() {
         DBInfoReader dbInfoReader = new DBInfoReader();
+        connection = createDBConnection(dbInfoReader);
+    }
+
+    private ToDoList getToDoList(String user_name, String list_name) {
+        Integer list_id = getListId(
+                getUserId(user_name),
+                list_name);
+        if (list_id == null) {
+            /*
+                TODO : Can't find a list with this list_name for user_name (Either here or in getListId())
+             */
+            return null;
+        }
+        return new ToDoList(list_id, list_name, getListItems(list_id));
+    }
+
+    private ArrayList<ToDoList> getToDoLists(String user_name) {
+        ArrayList<String> list_names = getListNames(user_name);
+        ArrayList<ToDoList> lists = new ArrayList<>();
+        for( String list_name : list_names) {
+            lists.add(getToDoList(user_name, list_name));
+        }
+
+        return lists;
+    }
+
+    private Integer getUserId(String userName) {
+        String query = "SELECT user_id FROM user WHERE user_name = '" + userName + "';";
         try {
-            connection = createDBConnection(dbInfoReader);
-            statement = connection.createStatement();
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery(query);
+            Integer i = null;
+            if(rs.next()) {
+                i=rs.getInt("user_id");
+            }
+            /*
+                TODO : if there is no user_id, i is NULL, and will not be interpreted as null but as 0 later
+             */
+            statement.close();
+            return i;
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("SQL Query error : getUserId", e);
         }
     }
 
-    private HashMap<Integer, String> getListNamesForUser(String user_name) {
-        String query = "SELECT list_id, label\n" +
+    private Integer getListId(Integer user_id, String list_name) {
+        String query1 = "SELECT list_id FROM lists WHERE user_id = '"+user_id+"';";
+        String query2 = "SELECT list_id FROM list_name WHERE label = '"+list_name+"';";
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery(query1);
+            ArrayList<Integer> user_list_ids = new ArrayList<>();
+            while(rs.next()) {
+                user_list_ids.add(rs.getInt("list_id"));
+
+            }
+            ArrayList<Integer> name_list_ids = new ArrayList<>();
+            rs = statement.executeQuery(query2);
+            while(rs.next()) {
+                name_list_ids.add(rs.getInt("list_id"));
+            }
+
+            ArrayList<Integer> list_ids = new ArrayList<>(user_list_ids);
+            list_ids.retainAll(name_list_ids);
+
+            statement.close();
+            return !list_ids.isEmpty() ? list_ids.getFirst() : null;
+
+        } catch (SQLException e) {
+            throw new RuntimeException("SQL Query error : getListId", e);
+        }
+    }
+
+    private ArrayList<ListItem> getListItems(Integer list_id) {
+        String query = "SELECT item_id, label, is_checked\n" +
+                "FROM items\n" +
+                "WHERE item_id IN (\n" +
+                "\tSELECT item_id\n" +
+                "\tFROM list_items\n" +
+                "\tWHERE list_id = "+list_id+"\n" +
+                ");";
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery(query);
+            ArrayList<ListItem> listItems = new ArrayList<>();
+
+            while (rs.next()) {
+                Integer item_id = rs.getInt("item_id");
+                String label = rs.getString("label");
+                Boolean is_checked = rs.getBoolean("is_checked");
+                listItems.add(
+                        new ListItem(item_id, label, is_checked));
+            }
+
+            statement.close();
+            return listItems;
+        } catch (SQLException e) {
+            throw new RuntimeException("SQL Query error : getListItemNames", e);
+        }
+    }
+
+    private ArrayList<String> getListNames(String user_name) {
+        String query = "SELECT label\n" +
                 "FROM list_name\n" +
                 "WHERE list_id IN (\n" +
                 "\tSELECT list_id\n" +
@@ -30,17 +122,18 @@ public class ToDoListServer {
                 "\t\tWHERE user.user_name = '"+user_name+"'\n" +
                 "));";
         try {
+            Statement statement = connection.createStatement();
             ResultSet rs = statement.executeQuery(query);
-            HashMap<Integer, String> listNames = new HashMap<>();
+            ArrayList<String> listNames = new ArrayList<>();
             while (rs.next()) {
-                int list_id = rs.getInt("list_id");
                 String label = rs.getString("label");
-                listNames.put(list_id, label);
+                listNames.add(label);
             }
 
+            statement.close();
             return listNames;
         } catch (SQLException e) {
-            throw new RuntimeException("SQL Query error : getListNamesFroUser", e);
+            throw new RuntimeException("SQL Query error : getListNames", e);
         }
     }
 
@@ -58,12 +151,46 @@ public class ToDoListServer {
         }
     }
 
+    public void closeDBConnection() throws SQLException {
+        connection.close();
+    }
+
     public static void main(String[] args) {
         ToDoListServer serv = new ToDoListServer();
 
-        HashMap<Integer, String> listNames = serv.getListNamesForUser("Bob");
-        for(Integer list_id : listNames.keySet()){
-            System.out.println(list_id + " " + listNames.get(list_id));
+
+        System.out.println("Getting Bob's to do lists :");
+
+        ArrayList<ToDoList> todolists = serv.getToDoLists("Bob");
+        for(ToDoList todolist : todolists) {
+            System.out.println("+" + todolist.getLabel());
+            for(ListItem item : todolist.getItems()) {
+                if (item.isChecked()){
+                    System.out.println("X-" + item.getLabel());
+                } else {
+                    System.out.println("--" + item.getLabel());
+                }
+            }
+        }
+
+        System.out.println("Getting John's list named Groceries :");
+
+        ToDoList johntodo = serv.getToDoList("John", "Groceries");
+        if (johntodo!=null) {
+            System.out.println("+" + johntodo.getLabel());
+            for (ListItem item : johntodo.getItems()) {
+                if (item.isChecked()) {
+                    System.out.println("X-" + item.getLabel());
+                } else {
+                    System.out.println("--" + item.getLabel());
+                }
+            }
+        }
+
+        try {
+            serv.closeDBConnection();
+        } catch (SQLException e) {
+            throw new RuntimeException("Couldn't properly close DB connection", e);
         }
     }
 }
